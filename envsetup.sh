ff806b2eb7f4264e349dfcfbf1d38a263852c210
aways <aways@c-rom.org>
2014-02-02 01:36:00 +0100

    TEST

--- envsetup.sh
+++ envsetup.sh
@@ -116,7 +116,7 @@ function setpaths()
     #   This function sets ANDROID_BUILD_PATHS to what it is adding  #
     #   to PATH, and the next time it is run, it removes that from   #
     #   PATH.  This is required so lunch can be run more than once   #
-    #   and still have working paths.                                #
+    #   and still have working paths..                                #
     #                                                                #
     ##################################################################
 
